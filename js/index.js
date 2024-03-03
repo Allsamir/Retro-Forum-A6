db868e33be94e499fa9a5492d7a5ce9aa6a03298
Al Samir <shahinur1591@gmail.com>
2024-03-03 20:58:40 +0600

    Navbar done

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ tailwind.config = {
       theme: {
         extend: {
           colors: {
-            
+            buttonBg: '#797DFC',
+            navbarButtonBorder: 'rgba(3, 7, 18, 0.10)',
+            navbarButtonBg: 'rgba(18, 19, 45, 0.03)',
+            bannerBg: '#12132D',
           }
         }
       }
